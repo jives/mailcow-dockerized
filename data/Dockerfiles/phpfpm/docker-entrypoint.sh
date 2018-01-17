2f3d322728e38e7b376297422a8d2684d516e1c6
Anton Kerschbaum <jives@gmx.net>
2018-01-17 19:01:08 +0100

    arm64: Minor updates and improvements

--- data/Dockerfiles/phpfpm/docker-entrypoint.sh
+++ data/Dockerfiles/phpfpm/docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 if [[ ! -d "/data/dkim/txt" || ! -d "/data/dkim/keys" ]] ; then mkdir -p /data/dkim/{txt,keys} ; chown -R www-data:www-data /data/dkim; fi
 if [[ $(stat -c %U /data/dkim/) != "www-data" ]] ; then chown -R www-data:www-data /data/dkim ; fi
+if [[ $(stat -c %U /srv/) != "www-data" ]] ; then chown -R www-data:www-data /srv ; fi
 
 # Wait for containers
 
